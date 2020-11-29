ea263f1e581070f0c2e4516b810beeff4562f162
Zoia Apostoloska <zoiaapostoloska@gmail.com>
2020-11-29 19:42:34 +0100

    исправляет задание 1

--- js/task-4.js
+++ js/task-4.js
@@ -1,16 +1,16 @@
-const credits = 23580;
-const pricePerDroid = 3000;
-const droidQuantity = prompt ('Введите количество дроидов, которое хотите купить');
-const totalPrice = pricePerDroid * droidQuantity;
-const creditsBalance = credits - totalPrice;
-let message;
+// const credits = 23580;
+// const pricePerDroid = 3000;
+// const droidQuantity = prompt ('Введите количество дроидов, которое хотите купить');
+// const totalPrice = pricePerDroid * droidQuantity;
+// const creditsBalance = credits - totalPrice;
+// let message;
 
-if (droidQuantity === null) {
-    message = 'Отменено пользователем!';
-}  else if (totalPrice > credits) {
-    message = 'Недостаточно средств на счету!';
-} else {
-    message = `Вы купили ${droidQuantity} дроидов, на счету осталось ${creditsBalance} кредитов.`;
-}
+// if (droidQuantity === null) {
+//     message = 'Отменено пользователем!';
+// }  else if (totalPrice > credits) {
+//     message = 'Недостаточно средств на счету!';
+// } else {
+//     message = `Вы купили ${droidQuantity} дроидов, на счету осталось ${creditsBalance} кредитов.`;
+// }
 
-console.log(message);
+// console.log(message);
--- js/task-5.js
+++ js/task-5.js
@@ -1,49 +1,49 @@
-let country = prompt('В какую страну должна быть осуществлена доставка?');
+// let country = prompt('В какую страну должна быть осуществлена доставка?');
 
-if (country !== null) {
-    country = country.toLowerCase();
-}
+// if (country !== null) {
+//     country = country.toLowerCase();
+// }
 
-let cost;
-let message = 'Выберите другую страну';
+// let cost;
+// let message = 'Выберите другую страну';
 
-switch (country) {
+// switch (country) {
 
-    case 'китай':
-    cost = 100;
-    country = 'в Китай';
-    break;
+//     case 'китай':
+//     cost = 100;
+//     country = 'в Китай';
+//     break;
 
-    case 'чили':
-    cost = 250;
-    country = 'в Чили';
-    break;
+//     case 'чили':
+//     cost = 250;
+//     country = 'в Чили';
+//     break;
 
-    case 'австралия':
-    cost = 170;
-    country = 'в Австралию';
-    break;
+//     case 'австралия':
+//     cost = 170;
+//     country = 'в Австралию';
+//     break;
 
-    case 'индия':
-    cost = 80;
-    country = 'в Индию';
-    break;
+//     case 'индия':
+//     cost = 80;
+//     country = 'в Индию';
+//     break;
 
-    case 'ямайка':
-    cost = 120;
-    country = 'на Ямайку';
-    break;
+//     case 'ямайка':
+//     cost = 120;
+//     country = 'на Ямайку';
+//     break;
 
-    default:
-    if (country !== null) {
-    alert('В вашей стране доставка не доступна.')};
-}
+//     default:
+//     if (country !== null) {
+//     alert('В вашей стране доставка не доступна.')};
+// }
 
-if (cost) {
-    message =`Доставка ${country} будет стоить ${cost} кредитов`;
-}
+// if (cost) {
+//     message =`Доставка ${country} будет стоить ${cost} кредитов`;
+// }
 
-console.log (message);
+// console.log (message);
 
 
 
